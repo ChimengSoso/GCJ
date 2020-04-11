a2299f5af67264c6db9568c933a0ad94c5159bb0
ChimengSoso <meng741258@gmail.com>
2020-04-11 20:14:35 +0700

    add code

--- 2020_Qualification/01_Vestigium.cpp
+++ 2020_Qualification/01_Vestigium.cpp
@@ -12,18 +12,19 @@ int main() {
   for (int t = 1; t <= tc; ++t) {
     int n; scanf("%d", &n);
     
-    int row[n][n] = {}, col[n][n] = {}, nr[n] = {}, nc[n] = {}, k{};
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
+    int row[n+1][n+1] = {}, nr[n+1] = {},
+        col[n+1][n+1] = {}, nc[n+1] = {}, k{};
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= n; ++j) {
         int m; scanf("%d", &m);
-        if (i == j) k += m; --m;
+        if (i == j) k += m;
         if (++row[i][m] >= 2) nr[i] = 1;
         if (++col[j][m] >= 2) nc[j] = 1;
       }
     }
     
-    int r = accumulate(nr, nr+n, 0),
-        c = accumulate(nc, nc+n, 0);
+    int r = accumulate(nr+1, nr+n+1, 0),
+        c = accumulate(nc+1, nc+n+1, 0);
     printf("Case #%d: %d %d %d\n", t, k, r, c);
   }
   return 0;
